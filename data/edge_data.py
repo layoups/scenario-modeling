502fd6edf0d7cbcab2f85037a30b6a89e4e28d1a
layoups <layounkarim@gmail.com>
2021-07-14 13:22:56 -0400

    lat is before long

--- data/edge_data.py
+++ data/edge_data.py
@@ -10,10 +10,11 @@ from AutoMap import *
 from math import radians, cos, sin, asin, sqrt
 
 def haversine_distance(lat1, lon1, lat2, lon2):
-    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
 
-    dlon = lon2 - lon1 
     dlat = lat2 - lat1 
+    dlon = lon2 - lon1 
+
     a = sin(dlat/2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon/2) ** 2
     c = 2 * asin(sqrt(a)) 
     r = 6372.8 
@@ -27,4 +28,4 @@ if __name__ == '__main__':
     lon2 = 2.3522
     lat2 = 48.8566
 
-    print(haversine_distance(lon1, lat1, lon2, lat2))
+    print(haversine_distance(lat1, lon1, lat2, lon2))
