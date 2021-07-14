9504bb01c38f3fc6f991da6906e9def58555982c
layoups <layounkarim@gmail.com>
2021-07-14 13:14:15 -0400

    haversine distances

--- data/edge_data.py
+++ data/edge_data.py
@@ -7,4 +7,24 @@ from sqlalchemy.sql import func
 
 from AutoMap import *
 
-### lets get some edges
+from math import radians, cos, sin, asin, sqrt
+
+def haversine_distance(lat1, lon1, lat2, lon2):
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon/2) ** 2
+    c = 2 * asin(sqrt(a)) 
+    r = 6372.8 
+    return c * r
+
+
+if __name__ == '__main__':
+
+    lon1 = 84.3880
+    lat1 = 33.7490
+    lon2 = 2.3522
+    lat2 = 48.8566
+
+    print(haversine_distance(lon1, lat1, lon2, lat2))
