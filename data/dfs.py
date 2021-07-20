206f384eb79a32e54e8985d3235e7bf7097f2107
layoups <klayoun@cisco.com>
2021-07-20 22:06:17 +0000

    baseline creation + raw nrp data

--- data/dfs.py
+++ data/dfs.py
@@ -16,13 +16,22 @@ from Alpha import *
 
 Session = sessionmaker(bind=engine)
 
-def create_baseline(baseline_id, start, end, description, date, session):
+def create_baseline(baseline_id, start, end, description, session):
+    date = datetime.now()
+
     baseline = Baselines(baseline_id=baseline_id)
     baseline.start = start
     baseline.end = end
     baseline.description = description
     baseline.date = date
     session.add(baseline)
+
+    scenario = Scenarios(scenario_id=0)
+    scenario.baseline_id = baseline_id
+    scenario.date = date
+    scenario.description = description
+    session.add(scenario)
+
     session.commit()
 
 def create_scenario(scenario_id, baseline_id, date, description, session):
@@ -34,12 +43,12 @@ def create_scenario(scenario_id, baseline_id, date, description, session):
     session.commit()
 
 
-def populate_scenario_lanes(scenario_id, baseline_id, session):
+def populate_scenario_lanes(baseline_id, session):
     baseline = session.query(Baselines).filter(Baselines.baseline_id == baseline_id).first()
 
     stmt = text("""
         insert into "SCDS_DB"."SCDS_SCDSI_WI"."SCDSI_SCENARIO_LANES" (scenario_row_id, scenario_id, baseline_id, pdct_fam, ori_name, ori_country, ori_region, desti_name, desti_country, desti_region, ship_type, ship_rank, total_weight, total_paid)
-        select "ROW_ID", :scenario_id, :baseline_id, "PRODUCT_FAMILY", 
+        select "ROW_ID", 0, :baseline_id, "PRODUCT_FAMILY", 
         lower("SHIP_FROM_NAME"), lower("SHIP_FROM_COUNTRY"), lower("SHIP_FROM_REGION_CODE"),
         lower("SHIP_TO_NAME"), lower("SHIP_TO_COUNTRY"), lower("SHIP_TO_REGION_CODE"),
         "SHIPMENT_TYPE", ship_rank,
@@ -54,13 +63,12 @@ def populate_scenario_lanes(scenario_id, baseline_id, session):
         "SHIP_FROM_NAME", "SHIP_FROM_COUNTRY", "SHIP_FROM_REGION_CODE", 
         "SHIP_TO_NAME", "SHIP_TO_COUNTRY", "SHIP_TO_REGION_CODE", 
         "SHIPMENT_TYPE", ship_rank;
-    """).params(scenario_id = scenario_id, baseline_id = baseline_id, start = baseline.start, end = baseline.end)
+    """).params(baseline_id = baseline_id, start = baseline.start, end = baseline.end)
 
     session.execute(stmt)
-    print(session.dirty)
     session.commit()
 
-def dfs(scenario_id, baseline_id, pdct_fam):
+def dfs(baseline_id, pdct_fam):
     print('Network Reconstruction for {}'.format(pdct_fam))
 
     session = Session()
@@ -93,19 +101,19 @@ def dfs(scenario_id, baseline_id, pdct_fam):
             path_rank = 0
             curr_path_head_rank = [-1]
             stack += [v]
-            dfs_visit(scenario_id, baseline_id, pdct_fam, stack, pflow, path_stack, curr_path_head_rank, path, path_rank, time, pflow_heads, session)
+            dfs_visit(baseline_id, pdct_fam, stack, pflow, path_stack, curr_path_head_rank, path, path_rank, time, pflow_heads, session)
         pflow += 1
         # input("new pflow-----------------------------------------------------------------------new pflow")
     print("Network Revealed for {}".format(pdct_fam))
     print(datetime.now() - start)
 
 
-def dfs_visit(scenario_id, baseline_id, pdct_type, stack, pflow, path_stack, curr_path_head_rank, path, path_rank, time, pflow_heads, session):
+def dfs_visit(baseline_id, pdct_type, stack, pflow, path_stack, curr_path_head_rank, path, path_rank, time, pflow_heads, session):
     v = stack[-1]
 
     # debug_print(stack, path, path_stack, path_rank, pflow, curr_path_head_rank[0], "HEAD")
 
-    v.scenario_id = scenario_id
+    v.scenario_id = 0
     v.baseline_id = baseline_id
     v.pflow = pflow
     v.path = path[0]
@@ -160,18 +168,18 @@ def dfs_visit(scenario_id, baseline_id, pdct_type, stack, pflow, path_stack, cur
 
 if __name__ == "__main__":
 
-    print(ScenarioLanes.__table__.columns.keys())
+    # print(ScenarioLanes.__table__.columns.keys())
 
     pdct_fam = "4400ISR"
     scenario_id = 0
     baseline_id = 1
 
-    # create_baseline(1, '2020-01-01', datetime.now(), 'trial', '2021-07-20', Session())
-    # create_scenario(0, 1, datetime.now(), 'baseline', Session())
+    # create_baseline(baseline_id, '2020-01-01', '2020-12-31', 'trial', Session())
+    # populate_scenario_lanes(baseline_id, Session())
 
-    populate_scenario_lanes(scenario_id, baseline_id, Session())
     # erase([pdct_fam], Session(), ScenarioLanes)
-    # dfs(scenario_id, baseline_id, pdct_fam)
+    
+    dfs(scenario_id, baseline_id, pdct_fam)
     # get_alphas(pdct_fam, Session())
     # visualize_networkx(pdct_fam, Session())
     # visualize_graphivz(pdct_fam, Session())
