c9420d20b298e42fde4b189156cf0742d06b343e
layoups <klayoun@cisco.com>
2021-07-21 22:04:27 +0000

    final scenario edges automap

--- data/AutoMap.py
+++ data/AutoMap.py
@@ -353,6 +353,22 @@ class Locations(Base):
 class ScenarioEdges(Base):
 
     scenario_edge_id = Column(Integer, primary_key=True)
+    baseline_id = Column('baseline_id', String, ForeignKey('scdsi_baselines.baseline_id'))
+    scenario_id = Column('scenario_id', Integer, ForeignKey('scdsi_scenarios.scenario_id'))
+    pdct_fam = Column('pdct_fam', String)
+    ori_name = Column('ori_name', String)
+    ori_country = Column('ori_country', String)
+    ori_region = Column('ori_region', String)
+    ori_role = Column('ori_role', String, default='')
+    desti_name = Column('desti_name', String)
+    desti_country = Column('desti_country', String)
+    desti_region = Column('desti_region', String)
+    desti_role = Column('desti_role', String, default='')
+    transport_mode = Column('transport_mode', String)
+    transport_time = Column('transport_time', Float)
+    co2e = Column('co2e', Float)
+    transport_cost = Column('transport_cost', Float)
+    distance = Column('distance', Float)
 
     __tablename__ = 'scdsi_scenario_edges'
     __table_args__ = {'extend_existing': True}
