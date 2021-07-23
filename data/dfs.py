cdf2c67ce6212e8be7ef43c3f39ca56eeed2171e
layoups <klayoun@cisco.com>
2021-07-23 16:19:00 +0000

    seperate dfs from baseline

--- data/dfs.py
+++ data/dfs.py
@@ -14,60 +14,6 @@ from Eraser import *
 from Visualize import *
 from Alpha import *
 
-Session = sessionmaker(bind=engine)
-
-def create_baseline(baseline_id, start, end, description, session):
-    date = datetime.now()
-
-    baseline = Baselines(baseline_id=baseline_id)
-    baseline.start = start
-    baseline.end = end
-    baseline.description = description
-    baseline.date = date
-    session.add(baseline)
-
-    scenario = Scenarios(scenario_id=0)
-    scenario.baseline_id = baseline_id
-    scenario.date = date
-    scenario.description = description
-    session.add(scenario)
-
-    session.commit()
-
-def create_scenario(scenario_id, baseline_id, date, description, session):
-    scenario = Scenarios(scenario_id=scenario_id)
-    scenario.baseline_id = baseline_id
-    scenario.date = date
-    scenario.description = description
-    session.add(scenario)
-    session.commit()
-
-
-def populate_scenario_lanes(baseline_id, session):
-    baseline = session.query(Baselines).filter(Baselines.baseline_id == baseline_id).first()
-
-    stmt = text("""
-        insert into "SCDS_DB"."SCDS_SCDSI_WI"."SCDSI_SCENARIO_LANES" (scenario_row_id, scenario_id, baseline_id, pdct_fam, ori_name, ori_country, ori_region, desti_name, desti_country, desti_region, ship_type, ship_rank, total_weight, total_paid)
-        select "ROW_ID", 0, :baseline_id, "PRODUCT_FAMILY", 
-        lower("SHIP_FROM_NAME"), lower("SHIP_FROM_COUNTRY"), lower("SHIP_FROM_REGION_CODE"),
-        lower("SHIP_TO_NAME"), lower("SHIP_TO_COUNTRY"), lower("SHIP_TO_REGION_CODE"),
-        "SHIPMENT_TYPE", ship_rank,
-        sum("BILLED_WEIGHT"), sum("TOTAL_AMOUNT_PAID")
-        from "SCDS_DB"."SCDS_SCDSI_STG"."SCDSI_CV_LANE_RATE_AUTOMATION_PL" rl join "SCDS_DB"."SCDS_SCDSI_WI"."SCDSI_SHIP_RANK" sr 
-        on rl."SHIPMENT_TYPE" = sr.ship_type
-        where "BILLED_WEIGHT" != 0 
-        and "SHIPMENT_TYPE" not in ('OTHER', 'BROKERAGE')
-        and "SHIP_DATE_PURE_SHIP" >= :start and "SHIP_DATE_PURE_SHIP" <= :end
-        and "PRODUCT_FAMILY" not in ('TBA')
-        group by "ROW_ID", "PRODUCT_FAMILY", 
-        "SHIP_FROM_NAME", "SHIP_FROM_COUNTRY", "SHIP_FROM_REGION_CODE", 
-        "SHIP_TO_NAME", "SHIP_TO_COUNTRY", "SHIP_TO_REGION_CODE", 
-        "SHIPMENT_TYPE", ship_rank;
-    """).params(baseline_id = baseline_id, start = baseline.start, end = baseline.end)
-
-    session.execute(stmt)
-    session.commit()
-
 def dfs(baseline_id, pdct_fam):
     print('Network Reconstruction for {}'.format(pdct_fam))
 
@@ -168,6 +114,8 @@ def dfs_visit(baseline_id, pdct_type, stack, pflow, path_stack, curr_path_head_r
 
 
 if __name__ == "__main__":
+    
+    Session = sessionmaker(bind=engine)
 
     # print(ScenarioLanes.__table__.columns.keys())
 
