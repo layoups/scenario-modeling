23ceec33dfa81bb0e1b145dd81075ad65abfaa51
layoups <klayoun@cisco.com>
2021-07-28 23:46:20 +0000

    capital letters baseline

--- data/baseline.py
+++ data/baseline.py
@@ -41,22 +41,22 @@ def populate_scenario_lanes(baseline_id, session):
     baseline = session.query(Baselines).filter(Baselines.baseline_id == baseline_id).first()
 
     stmt = text("""
-        insert into "SCDS_DB"."SCDS_SCDSI_WI"."SCDSI_SCENARIO_LANES" (scenario_row_id, scenario_id, baseline_id, pdct_fam, ori_name, ori_country, ori_region, desti_name, desti_country, desti_region, ship_type, ship_rank, total_weight, total_paid)
-        select "ROW_ID", 0, :baseline_id, "PRODUCT_FAMILY", 
-        lower("SHIP_FROM_NAME"), lower("SHIP_FROM_COUNTRY"), lower("SHIP_FROM_REGION_CODE"),
-        lower("SHIP_TO_NAME"), lower("SHIP_TO_COUNTRY"), lower("SHIP_TO_REGION_CODE"),
-        "SHIPMENT_TYPE", ship_rank,
-        sum("BILLED_WEIGHT"), sum("TOTAL_AMOUNT_PAID_USD")
-        from "SCDS_DB"."SCDS_SCDSI_STG"."SCDSI_CV_LANE_RATE_AUTOMATION_PL" rl join "SCDS_DB"."SCDS_SCDSI_WI"."SCDSI_SHIP_RANK" sr 
-        on rl."SHIPMENT_TYPE" = sr.ship_type
-        where "BILLED_WEIGHT" != 0 
-        and "SHIPMENT_TYPE" not in ('OTHER', 'BROKERAGE')
-        and "SHIP_DATE_PURE_SHIP" >= :start and "SHIP_DATE_PURE_SHIP" <= :end
-        and "PRODUCT_FAMILY" not in ('TBA')
-        group by "ROW_ID", "PRODUCT_FAMILY", 
-        "SHIP_FROM_NAME", "SHIP_FROM_COUNTRY", "SHIP_FROM_REGION_CODE", 
-        "SHIP_TO_NAME", "SHIP_TO_COUNTRY", "SHIP_TO_REGION_CODE", 
-        "SHIPMENT_TYPE", ship_rank;
+        insert into scdsi_scenario_lanes (scenario_row_id, scenario_id, baseline_id, pdct_fam, ori_name, ori_country, ori_region, desti_name, desti_country, desti_region, ship_type, ship_rank, total_weight, total_paid)
+        select row_id, 0, :baseline_id, product_family, 
+        lower(ship_from_name), lower(ship_from_country), lower(ship_from_region_code),
+        lower(ship_to_name), lower(ship_to_country), lower(ship_to_region_code),
+        shipment_type, ship_rank,
+        sum(billed_weight), sum(total_amount_paid_usd)
+        from scdsi_cv_lane_rate_automation rl join scdsi_ship_rank sr 
+        on rl.shipment_type = sr.ship_type
+        where billed_weight != 0 
+        and shipment_type not in ('OTHER', 'BROKERAGE')
+        and ship_date_pure_ship >= :start and ship_date_pure_ship <= :end
+        and product_family not in ('TBA')
+        group by row_id, product_family, 
+        ship_from_name, ship_from_country, ship_from_region_code, 
+        ship_to_name, ship_to_country, ship_to_region_code, 
+        shipment_type, ship_rank;
     """).params(baseline_id = baseline_id, start = baseline.start, end = baseline.end)
 
     session.execute(stmt)
@@ -64,10 +64,10 @@ def populate_scenario_lanes(baseline_id, session):
 
 def get_cost_omega(baseline_id, session):
     stmt = text("""
-        insert into "SCDS_DB"."SCDS_SCDSI_WI"."SCDSI_OMEGA" ("BASELINE_ID", "OMEGA_COST")
-        select "BASELINE_ID", sum("TOTAL_WEIGHT")
-        from "SCDS_DB"."SCDS_SCDSI_WI"."SCDSI_SCENARIO_LANES"
-        where "BASELINE_ID" = :baseline_id
+        insert into scdsi_omega (baseline_id, omega_cost)
+        select baseline_id, sum(total_weight)
+        from scdsi_scenario_lanes
+        where baseline_id = :baseline_id
     """).params(baseline_id = baseline_id)
 
     session.execute(stmt)
