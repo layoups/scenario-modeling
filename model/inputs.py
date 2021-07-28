e2ddcc798aad8ff2bb062a5788908dc8fa93e70f
layoups <layounkarim@gmail.com>
2021-07-28 15:03:26 -0400

    mode inputs

--- model/inputs.py
+++ model/inputs.py
@@ -1,5 +1,18 @@
 import numpy as np
 import pandas as pd
 
-X = {} # (i, j, p, m): {'adj: [(k, i, p, m)], 'transport_time'}
+X = {}  
+{
+    (i, j, p): {
+        'alpha': ,
+        'adj': [[(k, i, p), (k_1, i, p)], [(l, i, p)]], 
+        'transport_modes': {
+            'mode': {
+                'transport_time': ,
+                'transport_cost': ,
+                'transport_co2e': 
+            }
+        }
+    }
+}
 Q = {} # (n, p): {'capacity': , 'supply': , 'opex': }
