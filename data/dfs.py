a9408dd2b6901347c64be6798b1b2f530792056a
layoups <klayoun@cisco.com>
2021-07-29 16:06:39 +0000

    viz update

--- data/dfs.py
+++ data/dfs.py
@@ -118,17 +118,17 @@ if __name__ == "__main__":
 
     # print(ScenarioLanes.__table__.columns.keys())
 
-    pdct_fam = "AIRANT"
+    pdct_fam = "WPHONE"
     scenario_id = 0
     baseline_id = 1
 
     # create_baseline(baseline_id, '2020-01-01', '2020-12-31', 'trial', Session())
     # populate_scenario_lanes(baseline_id, Session())
 
-    # erase([pdct_fam], scenario_id, baseline_id, Session(), ScenarioLanes)
+    erase([pdct_fam], scenario_id, baseline_id, Session(), ScenarioLanes)
     
-    # dfs(baseline_id, pdct_fam, Session())
-    # get_customer_alphas(scenario_id, baseline_id, pdct_fam, Session())
+    dfs(baseline_id, pdct_fam, Session())
+    get_customer_alphas(scenario_id, baseline_id, pdct_fam, Session())
     get_alphas(scenario_id, baseline_id, pdct_fam, Session())
     # visualize_networkx(pdct_fam, Session())
     visualize_graphivz(scenario_id, baseline_id, pdct_fam, Session())
