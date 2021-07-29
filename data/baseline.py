892e91d1ce0633d5c16f63a161dd5b23d5b56525
layoups <klayoun@cisco.com>
2021-07-29 01:13:12 +0000

    more optimization needed for populate scenario lanes query

--- data/baseline.py
+++ data/baseline.py
@@ -42,31 +42,34 @@ def populate_scenario_lanes(baseline_id, session):
 
     stmt = text("""
         insert into scdsi_scenario_lanes 
-        (scenario_row_id, scenario_id, baseline_id, pdct_fam, 
+        (scenario_id, baseline_id, pdct_fam, 
         ori_name, ori_country, ori_region, 
         desti_name, desti_country, desti_region, 
         ship_type, ship_rank, total_weight, total_paid)
-        select row_id, 0, :baseline_id, product_family, 
+        select 0, :baseline_id
+        (select product_family, 
         lower(ship_from_name), lower(ship_from_country), lower(ship_from_region_code),
         lower(ship_to_name), lower(ship_to_country), lower(ship_to_region_code),
-        shipment_type, ship_rank,
+        shipment_type,
         sum(billed_weight), sum(total_amount_paid_usd)
-        from scdsi_cv_lane_rate_automation_pl rl join scdsi_ship_rank sr 
-        on rl.shipment_type = sr.ship_type
+        from scdsi_cv_lane_rate_automation_pl 
         where billed_weight != 0 
         and shipment_type not in ('OTHER', 'BROKERAGE')
-        and ship_date_pure_ship >= :start and ship_date_pure_ship <= :end
+        and ship_date_pure_ship >= '2020-01-01' and ship_date_pure_ship <= '2020-12-30'
         and product_family not in ('TBA')
         and ship_from_name is not null
         and ship_from_country is not null
         and ship_from_region_code is not null
+        and length(ship_from_name) > 3
         and ship_to_name is not null
         and ship_to_country is not null
         and ship_to_region_code is not null
-        group by row_id, product_family, 
+        and length(ship_to_name) > 3
+        group by product_family, 
         ship_from_name, ship_from_country, ship_from_region_code, 
         ship_to_name, ship_to_country, ship_to_region_code, 
-        shipment_type, ship_rank;
+        shipment_type) as rl join scdsi_ship_rank sr 
+        on rl.shipment_type = sr.ship_type
     """).params(baseline_id = baseline_id, start = baseline.start, end = baseline.end)
 
     session.execute(stmt)
@@ -86,20 +89,20 @@ def get_cost_omega(baseline_id, session):
 def set_baseline(baseline_id, start, end, description, session):
     create_baseline(baseline_id, start, end, description, session)
     populate_scenario_lanes(baseline_id, session)
-
+    input()
     pdct_fam = 'PHONE'
     input('baseline + scenario lanes = ready for dfs?')
     dfs(baseline_id, pdct_fam, session)
 
-    populate_scenario_edges(0, baseline_id, session)
-    get_distances_time_co2e(0, baseline_id, session)
-    set_in_pflow_for_scenario_edges(0, baseline_id, session)
+    # populate_scenario_edges(0, baseline_id, session)
+    # get_distances_time_co2e(0, baseline_id, session)
+    # set_in_pflow_for_scenario_edges(0, baseline_id, session)
 
-    populate_baseline_nodes(baseline_id, session)
-    get_node_supply(0, baseline_id, pdct_fam, session)
-    get_node_capacity(0, baseline_id, pdct_fam, session)
+    # populate_baseline_nodes(baseline_id, session)
+    # get_node_supply(0, baseline_id, pdct_fam, session)
+    # get_node_capacity(0, baseline_id, pdct_fam, session)
 
-    get_cost_omega(baseline_id, session)
+    # get_cost_omega(baseline_id, session)
 
 
 if __name__ == '__main__':
