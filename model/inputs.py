2f8fdd0e1f523be013fb7b74f51051c26d6a570f
layoups <klayoun@cisco.com>
2021-07-30 22:36:41 +0000

    data dicts for opti

--- model/inputs.py
+++ model/inputs.py
@@ -3,3 +3,21 @@ import pandas as pd
 
 lanes = {}
 nodes = {}
+
+customer_lanes = {} 
+dslc_lanes = {} 
+oslc_lanes = {} 
+df_lanes = {} 
+ghub_lanes = {} 
+pcba_lanes = {}
+
+manufacturing_adjacency_list = {}
+
+C = {}
+V = {}
+E = {}
+T = {}
+
+S = {}
+U = {}
+alpha = {}
