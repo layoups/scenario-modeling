ea4deef75675129710d160e378711661b8019d03
layoups <klayoun@cisco.com>
2021-07-31 15:57:56 +0000

    populate scenario nodes filter for baseline id

--- data/node_data.py
+++ data/node_data.py
@@ -78,9 +78,11 @@ def populate_baseline_nodes(baseline_id, session):
     stmt = text("""
         insert into scdsi_scenario_nodes 
         (baseline_id, scenario_id, pdct_fam, name, country, region, role, in_pflow)
-        select distinct :baseline_id, 0, pdct_fam, ori_name, ori_country, ori_region, ori_role, 1
+        select distinct baseline_id, scenario_id, pdct_fam, ori_name, ori_country, ori_region, ori_role, 1
         from scdsi_scenario_lanes
         where in_pflow = 1
+        and baseline_id = baseline_id
+        and scenario_id = 0
     """).params(baseline_id = baseline_id)
 
     session.execute(stmt)
@@ -89,9 +91,12 @@ def populate_baseline_nodes(baseline_id, session):
     stmt = text("""
         insert into scdsi_scenario_nodes 
         (baseline_id, scenario_id, pdct_fam, name, country, region, role, in_pflow)
-        select distinct :baseline_id, 0, pdct_fam, desti_name, desti_country, desti_region, desti_role, 1
+        select distinct baseline_id, scenario_id, pdct_fam, desti_name, desti_country, desti_region, desti_role, 1
         from scdsi_scenario_lanes
-        where in_pflow = 1 and desti_role = 'Customer'
+        where in_pflow = 1 
+        and desti_role = 'Customer'
+        and baseline_id = baseline_id
+        and scenario_id = 0
     """).params(baseline_id = baseline_id)
 
     session.execute(stmt)
@@ -101,7 +106,8 @@ def get_node_supply(scenario_id, baseline_id, pdct_fam, session):
     nodes = session.query(ScenarioNodes).filter(
         ScenarioNodes.scenario_id == scenario_id,
         ScenarioNodes.baseline_id == baseline_id,
-        ScenarioNodes.pdct_fam == pdct_fam
+        ScenarioNodes.pdct_fam == pdct_fam,
+        ScenarioNodes.supply is not None
         )
 
     for n in nodes.all():
@@ -111,16 +117,18 @@ def get_node_supply(scenario_id, baseline_id, pdct_fam, session):
                 ScenarioLanes.baseline_id == n.baseline_id,
                 ScenarioLanes.desti_name == n.name,
                 ScenarioLanes.desti_country == n.country, 
-                ScenarioLanes.desti_region == n.region, 
+                # ScenarioLanes.desti_region == n.region, 
                 ScenarioLanes.desti_role == n.role,
                 ScenarioLanes.pdct_fam == n.pdct_fam,
                 ScenarioLanes.in_pflow == 1
                 ).group_by(
                     ScenarioLanes.desti_name,
                     ScenarioLanes.desti_country,
-                    ScenarioLanes.desti_region,
+                    # ScenarioLanes.desti_region,
                     ScenarioLanes.desti_role
                 ).first()
+            if (n.pdct_fam == 'SBPHONE'):
+                print(n)
             n.supply = -total.total
         else:
             n.supply = 0
@@ -187,7 +195,7 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    baseline_id = 1
+    baseline_id = 2
     scenario_id = 0
 
     pdct_fam = 'AIRANT'
@@ -201,9 +209,13 @@ if __name__ == '__main__':
     # populate_Locations(session)
     # get_lat_long(session)
 
-    populate_baseline_nodes(baseline_id, session)
-    get_node_supply(0, baseline_id, pdct_fam, session)
-    get_node_capacity(0, baseline_id, pdct_fam, session)
+    # populate_baseline_nodes(baseline_id, session)
+
+    pdct_fams = session.query(ScenarioLanes.pdct_fam).distinct().all()
+    for pdct_fam in pdct_fams:
+        print(pdct_fam[0])
+        get_node_supply(0, baseline_id, pdct_fam[0], session)
+        get_node_capacity(0, baseline_id, pdct_fam[0], session)
 
     print(datetime.now() - start)
     session.commit()
