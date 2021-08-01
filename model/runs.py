122f74a3a45131a1a55f4ac4a793b92f314c59e9
layoups <klayoun@cisco.com>
2021-08-01 21:30:33 +0000

    pareto weights

--- model/runs.py
+++ model/runs.py
@@ -1,5 +1,12 @@
 from AutoMap import *
 
+def get_pareto_weights():
+    ret = []
+    for i in np.arange(0, 1.1, 0.1):
+        for j in np.arange(0, 1 - i, 0.1):
+            ret += [(round(i, 2), round(j, 2), round(1 - i - j, 2))]
+
+    return ret
 
 
 if __name__ == '__main__':
@@ -7,22 +14,25 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session() 
 
-    scenario_id = 0
-    baseline_id = 1
+    pareto_weights = get_pareto_weights()
+    print(pareto_weights)
 
-    lambda_cost = 0.5
-    lambda_time = 0.25
-    lambda_co2e = 0.25
+    # scenario_id = 0
+    # baseline_id = 1
 
-    run = Runs(
-        scenario_id = scenario_id,
-        baseline_id = baseline_id,
-        date = datetime.now(),
-        description = 'first run',
-        lambda_cost = lambda_cost,
-        lambda_time = lambda_time,
-        lambda_co2e = lambda_co2e 
-    )
+    # lambda_cost = 0.5
+    # lambda_time = 0.25
+    # lambda_co2e = 0.25
 
-    session.add(run)
-    session.commit()
+    # run = Runs(
+    #     scenario_id = scenario_id,
+    #     baseline_id = baseline_id,
+    #     date = datetime.now(),
+    #     description = 'first run',
+    #     lambda_cost = lambda_cost,
+    #     lambda_time = lambda_time,
+    #     lambda_co2e = lambda_co2e 
+    # )
+
+    # session.add(run)
+    # session.commit()
