c000afbcf54caf973a28a693555684ecd5228ccf
layoups <klayoun@cisco.com>
2021-08-02 15:08:39 +0000

    model baseline 1

--- data/baseline.py
+++ data/baseline.py
@@ -161,8 +161,8 @@ def set_baseline(baseline_id, start, end, description, session):
         return session.commit()
     
     # pdct_fams = session.query(ScenarioLanes.pdct_fam).distinct().all()
-    # pdct_fams = [('AIRANT',), ('WPHONE',), ('SBPHONE',), ('PHONVOC',)]
-    pdct_fams = [('QSFP40G',)]
+    pdct_fams = [('AIRANT',), ('WPHONE',), ('SBPHONE',), ('PHONVOC',)]
+    # pdct_fams = [('QSFP40G',)]
 
     try:
         for pdct_fam in pdct_fams: 
@@ -271,8 +271,8 @@ if __name__ == '__main__':
 
     start = datetime.now()
 
-    baseline_id = 3
-    set_baseline(baseline_id, '2010-01-01', '2020-12-31', "mode trial one pdct" , session)
+    baseline_id = 1
+    set_baseline(baseline_id, '2019-01-01', '2020-12-31', "trial" , session)
 
     # get_cost_omega(baseline_id, session)
     # get_co2e_time_omega(baseline_id, session)
