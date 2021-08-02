0200ff5506d49dcd534b125b8309344f9b2bc8d2
layoups <klayoun@cisco.com>
2021-08-02 13:29:26 +0000

    all pareto weights

--- model/runs.py
+++ model/runs.py
@@ -1,12 +1,23 @@
 from AutoMap import *
 
+from gurobipy import GRB
+
 def get_pareto_weights():
     ret = []
-    for i in np.arange(0, 1.1, 0.1):
-        for j in np.arange(0, 1 - i, 0.1):
-            ret += [(round(i, 2), round(j, 2), round(1 - i - j, 2))]
+    for i in range(0, 11):
+        for j in range(0, 11 - i):
+            ret += [(i / 10., j / 10., (10 - i - j) / 10.)]
 
     return ret
+
+def print_optimal(model, index_to_node, index_to_mode):
+    if model.Status == GRB.OPTIMAL:
+        print("Variable Values")
+        for v in model.getVars():
+            if v.VarName[0] == 'X':
+                print('<({})> | {}'.format(v.VarName, v.X))
+
+
 
 
 if __name__ == '__main__':
@@ -20,19 +31,16 @@ if __name__ == '__main__':
     scenario_id = 0
     baseline_id = 3
 
-    lambda_cost = 1
-    lambda_time = 0
-    lambda_co2e = 0
+    for w in get_pareto_weights():
+        run = Runs(
+            scenario_id = scenario_id,
+            baseline_id = baseline_id,
+            date = datetime.now(),
+            description = 'one pf: QSFP40G',
+            lambda_cost = w[0],
+            lambda_time = w[1],
+            lambda_co2e = w[2] 
+        )
 
-    run = Runs(
-        scenario_id = scenario_id,
-        baseline_id = baseline_id,
-        date = datetime.now(),
-        description = 'first run',
-        lambda_cost = lambda_cost,
-        lambda_time = lambda_time,
-        lambda_co2e = lambda_co2e 
-    )
-
-    session.add(run)
+        session.add(run)
     session.commit()
