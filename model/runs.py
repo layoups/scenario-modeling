42bbad2a9ccf89df2d101d3755682dac3d52478c
layoups <klayoun@cisco.com>
2021-08-03 05:14:16 +0000

    flow siloed

--- model/runs.py
+++ model/runs.py
@@ -152,19 +152,19 @@ if __name__ == '__main__':
     scenario_id = 0
     baseline_id = 5
 
-    for w in get_pareto_weights():
-        run = Runs(
-            scenario_id = scenario_id,
-            baseline_id = baseline_id,
-            date = datetime.now(),
-            description = 'one pf: QSFP40G',
-            lambda_cost = w[0],
-            lambda_time = w[1],
-            lambda_co2e = w[2] 
-        )
+    # for w in get_pareto_weights():
+    #     run = Runs(
+    #         scenario_id = scenario_id,
+    #         baseline_id = baseline_id,
+    #         date = datetime.now(),
+    #         description = 'one pf: QSFP40G',
+    #         lambda_cost = w[0],
+    #         lambda_time = w[1],
+    #         lambda_co2e = w[2] 
+    #     )
 
-        session.add(run)
-    session.commit()
+    #     session.add(run)
+    # session.commit()
 
-    # pprint(get_mode_mix(scenario_id, baseline_id, session))
-    # pprint(getkpi_ranges(scenario_id, baseline_id, session))
+    pprint(get_mode_mix(scenario_id, baseline_id, session))
+    pprint(get_kpi_ranges(scenario_id, baseline_id, session))
